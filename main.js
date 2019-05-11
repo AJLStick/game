2ba63912318c7683003999af9816a0367585383e
AJLStick <somecrazystickfig04@gmail.com>
2019-05-11 14:20:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -24,3 +24,25 @@ window.setInterval(function(){
 	stickClick(axes);
 	
 }, 1000);
+
+};
+
+var axes = 0;
+
+function buyAxe(){
+    var axeCost = Math.floor(10 * Math.pow(1.1,chainsaws));     //works out the cost of this axe
+    if(sticks >= chainsawCost){                                   //checks that the player can afford the axe
+        chainsaws = chainsaws + 1;                                   //increases number of axes
+    	sticks = sticks - axeCost;                          //removes the sticks spent
+        document.getElementById('axes').innerHTML = chainsaws;  //updates the number of axes for the user
+        document.getElementById('sticks').innerHTML = sticks;  //updates the number of sticks for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,chainsaws));       //works out the cost of the next axe
+    document.getElementById('axeCost').innerHTML = nextCost;  //updates the axe cost for the user
+};
+
+window.setInterval(function(){
+	
+	stickClick(chainsaws);
+	
+}, 1000);
