a16d3aaca29f8a959d6f87ee642dfe7539cef760
AJLStick <somecrazystickfig04@gmail.com>
2019-05-13 08:38:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
 var sticks = 0;
 
-function stickClick(number){
-    sticks = sticks + number;
+function stickClick(){
+    sticks = sticks + 1;
     document.getElementById("sticks").innerHTML = sticks;
-};
-
-var axes = 0;
-
-function buyAxe(){
-    var axeCost = Math.floor(10 * Math.pow(1.1,axes));     //works out the cost of this axe
-    if(sticks >= axeCost){                                   //checks that the player can afford the axe
-        axes = axes + 1;                                   //increases number of axes
-    	sticks = sticks - axeCost;                          //removes the sticks spent
-        document.getElementById('axes').innerHTML = axes;  //updates the number of axes for the user
-        document.getElementById('sticks').innerHTML = sticks;  //updates the number of sticks for the user
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1,axes));       //works out the cost of the next axe
-    document.getElementById('axeCost').innerHTML = nextCost;  //updates the axe cost for the user
-};
-
-window.setInterval(function(){
-	
-	stickClick(axes);
-	
-}, 100);
-
 };
 
 var axes = 0;
