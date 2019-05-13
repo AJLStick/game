d05abe803bc808a213af8ea3ee2baf732857131b
AJLStick <somecrazystickfig04@gmail.com>
2019-05-13 08:29:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -24,3 +24,48 @@ window.setInterval(function(){
 	stickClick(axes);
 	
 }, 100);
+
+};
+
+var axes = 0;
+
+function buyAxe(){
+    var axeCost = Math.floor(10 * Math.pow(1.1,axes));     //works out the cost of this axe
+    if(sticks >= axeCost){                                   //checks that the player can afford the axe
+        axes = axes + 1;                                   //increases number of axes
+    	sticks = sticks - axeCost;                          //removes the sticks spent
+        document.getElementById('axes').innerHTML = axes;  //updates the number of axes for the user
+        document.getElementById('sticks').innerHTML = sticks;  //updates the number of sticks for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,axes));       //works out the cost of the next axe
+    document.getElementById('axeCost').innerHTML = nextCost;  //updates the axe cost for the user
+};
+
+window.setInterval(function(){
+	
+	stickClick(axes);
+	
+}, 900);
+
+};
+
+var chainsaws = 0;
+
+function buyChainsaw(){
+    var chainsawCost = Math.floor(10 * Math.pow(1.1,chainsaws));     //works out the cost of this axe
+    if(sticks >= chainsawCost){                                   //checks that the player can afford the axe
+        chainsaws = chainsaws + 1;                                   //increases number of axes
+    	sticks = sticks - chainsawCost;                          //removes the sticks spent
+        document.getElementById('axes').innerHTML = chainsaw;  //updates the number of axes for the user
+        document.getElementById('sticks').innerHTML = sticks;  //updates the number of sticks for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,chainsaws));       //works out the cost of the next axe
+    document.getElementById('axeCost').innerHTML = nextCost;  //updates the axe cost for the user
+};
+
+window.setInterval(function(){
+	
+	stickClick(chainsaws);
+	
+}, 500);
+
